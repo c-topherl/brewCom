8b33a838ecf4ac515dd015dd666a8572fc00c3d8
Joel <joelmeister1209@gmail.com>
2016-04-09 09:53:16 -0500

    fixed include

--- controllers/orders/submit_order.php
+++ controllers/orders/submit_order.php
@@ -7,7 +7,7 @@
 require_once("PDOConnection.php");
 include_once('orders/get_cart.php');
 include_once('orders/add_order.php');
-include_once('get_users.php');
+include_once('customers/get_users.php');
 include_once('mail.inc');
 
 function submit_order($values)
