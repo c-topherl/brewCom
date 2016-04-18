b8fdbd7290ed7fefe4bd4de72b3e8e31856cd629
Joel <joelmeister1209@gmail.com>
2016-04-18 01:11:13 -0500

    add/get/update address implemented

---
+++ controllers/customers/add_address.php
@@ -0,0 +1,74 @@
+<?php
+/*
+INPUTS:
+name
+address1
+address2 optional
+city
+state
+zipcode
+type - (0) billing / (1) shipping
+*/
+require_once "PDOConnection.php";
+function add_address($addressInfo)
+{
+    if(!(isset($addressInfo['name'])
+        && isset($addressInfo['address1'])
+        && isset($addressInfo['city'])
+        && isset($addressInfo['state'])
+        && isset($addressInfo['zipcode'])
+        && isset($addressInfo['type'])
+        && (isset($addressInfo['customer_id']) || isset($addressInfo['user_id']))))
+    {
+        throw new Exception("ERROR: name, address1, city, state, zipcode, type, and customer_id or user_id required");
+    }
+
+    $address2 = isset($addressInfo['address2']) ? $addressInfo['address2'] : '';
+
+    $dbh = new PDOConnection();
+
+    $query = "INSERT INTO addresses(name, address1, address2, city, state, zipcode, type) "
+            . "VALUES(:name, :address1, :address2, :city, :state, :zipcode, :type)";
+
+    $sth = $dbh->prepare($query);
+
+    $sth->bindParam(':name', $addressInfo['name']);
+    $sth->bindParam(':address1', $addressInfo['address1']);
+    $sth->bindParam(':address2', $address2);
+    $sth->bindParam(':city', $addressInfo['city']);
+    $sth->bindParam(':state', $addressInfo['state']);
+    $sth->bindParam(':zipcode', $addressInfo['zipcode']);
+    $sth->bindParam(':type', $addressInfo['type'], PDO::PARAM_INT);
+
+    if(!$sth->execute())
+    {
+        throw new Exception("ERROR: could not add address - " . $sth->errorInfo()[2]);
+    }
+
+    $address_id = $dbh->lastInsertId();
+
+    if(isset($addressInfo['user_id']))
+    {
+        $query = "INSERT INTO user_addresses(user_id,address_id) VALUES(:user_id, :address_id)";
+        $sth = $dbh->prepare($query);
+        $sth->bindParam(':user_id', $addressInfo['user_id'], PDO::PARAM_INT);
+        $sth->bindParam(':address_id', $address_id, PDO::PARAM_INT);
+        if(!$sth->execute())
+        {
+            throw new Exception("ERROR: could not add address - " . $sth->errorInfo()[2]);
+        }
+    }
+    elseif(isset($addressInfo['customer_id']))
+    {
+        $query = "INSERT INTO customer_addresses(customer_id,address_id) VALUES(:customer_id, :address_id)";
+        $sth = $dbh->prepare($query);
+        $sth->bindParam(':customer_id', $addressInfo['customer_id'], PDO::PARAM_INT);
+        $sth->bindParam(':address_id', $address_id, PDO::PARAM_INT);
+        if(!$sth->execute())
+        {
+            throw new Exception("ERROR: could not add address - " . $sth->errorInfo()[2]);
+        }
+    }
+
+    return array('id' => $address_id);
+}
---
+++ controllers/customers/update_address.php
@@ -0,0 +1,48 @@
+<?php
+/*
+INPUTS:
+customers_id/user_id
+name
+address1
+address2 optional
+city
+state
+zipcode
+type - (0) billing / (1) shipping
+*/
+require_once "PDOConnection.php";
+require_once "customers/get_addresses.php";
+function update_address($addressInfo)
+{
+    if(!(isset($addressInfo['address_id']) && (isset($addressInfo['customer_id']) || isset($addressInfo['user_id']))))
+    {
+        throw new Exception("ERROR: address_id and customer_id or user_id required");
+    }
+
+    $oldInfo = get_addresses($addressInfo)['addresses'][0]; //takes customer/user id and address id
+    if(empty($oldInfo))
+    {
+        throw new Exception("Could not find address for user");
+    }
+    $addressInfo = array_replace($oldInfo,$addressInfo);
+
+    $query = "UPDATE addresses SET name = :name, address1 = :address1, address2 = :address2, city = :city, state = :state, zipcode = :zipcode, type = :type WHERE id = :address_id";
+    $dbh = new PDOConnection();
+    $sth = $dbh->prepare($query);
+
+    $sth->bindParam(':name', $addressInfo['name']);
+    $sth->bindParam(':address1', $addressInfo['address1']);
+    $sth->bindParam(':address2', $addressInfo['address2']);
+    $sth->bindParam(':city', $addressInfo['city']);
+    $sth->bindParam(':state', $addressInfo['state']);
+    $sth->bindParam(':zipcode', $addressInfo['zipcode']);
+    $sth->bindParam(':type', $addressInfo['type'], PDO::PARAM_INT);
+    $sth->bindParam(':address_id', $addressInfo['address_id'], PDO::PARAM_INT);
+
+    if(!$sth->execute())
+    {
+        throw new Exception("ERROR: could not update address - " . $sth->errorInfo()[2]);
+    }
+
+    return $addressInfo;
+}
