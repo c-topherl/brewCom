c98b8ac4546a3fecdbbd8311589511b157025a2b
Joel <joelmeister1209@gmail.com>
2016-05-03 00:07:54 -0500

    accidentally added get warehouse logic.  need to include this file too

---
+++ controllers/products/get_warehouses.php
@@ -0,0 +1,35 @@
+<?php
+include_once('PDOConnection.php');
+function get_warehouses($opts = NULL)
+{
+    $dbh = new PDOConnection();
+
+    //not supported yet
+    $customer_id = isset($opts['customer_id']) ? $opts['customer_id'] : '';
+
+    //not supported yet
+    $query = "SELECT id, description, name FROM warehouses ";
+    if(isset($opts['id']))
+    {
+        $query .= " WHERE id = :id";
+    }
+
+    $sth = $dbh->prepare($query);
+
+    if(isset($opts['id']))
+    {
+        $sth->bindParam(':id', $opts['id'], PDO::PARAM_INT);
+    }
+
+    if(!$sth->execute())
+    {
+        throw new Exception($sth->errorInfo()[2]);
+    }
+
+    $warehouses = array();
+    foreach($sth->fetchAll(PDO::FETCH_ASSOC) as $row)
+    {
+        $warehouses[] = $row;
+    }
+    return $warehouses;
+}
