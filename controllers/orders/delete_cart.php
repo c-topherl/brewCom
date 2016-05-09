2f71adaa63b130ac96e37363951c07a616bfe858
Joel <joelmeister1209@gmail.com>
2016-05-08 23:52:55 -0500

    Implement delete_cart function

--- controllers/orders/delete_cart.php
+++ controllers/orders/delete_cart.php
@@ -2,6 +2,35 @@
 require_once "PDOConnection.php";
 include_once "orders/common_cart_functions.inc";
 
+function delete_cart($info)
+{
+    if(!isset($info['user_id']))
+    {
+        throw new Exception("user_id required.");
+    }
+    $dbh = new PDOConnection();
+    if(!check_cart_exists($dbh,$info['user_id']))
+    {
+        return "No cart found for user";
+    }
+    $query = "DELETE FROM cart_headers WHERE user_id = :user_id";
+    $sth = $dbh->prepare($query);
+    $sth->bindParam(':user_id', $info['user_id'], PDO::PARAM_INT);
+    if(!$sth->execute())
+    {
+        throw new Exception("delete_cart error: ".$sth->errorInfo()[2]);
+    }
+
+    $query = "DELETE FROM cart_details WHERE user_id = :user_id";
+    $sth = $dbh->prepare($query);
+    $sth->bindParam(':user_id', $info['user_id'], PDO::PARAM_INT);
+    if(!$sth->execute())
+    {
+        throw new Exception("delete_cart details error: ".$sth->errorInfo()[2]);
+    }
+    return TRUE;
+}
+
 function delete_cart_detail($cartDetail)
 {
     if(!(isset($cartDetail['user_id']) && isset($cartDetail['line_id'])))
